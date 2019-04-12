7e264d9c9af55bb63375e58d097e69f1d9a5fce9
Clair Rogers <clair-rogers@outlook.com>
2019-04-12 15:27:24 -0600

    initial setup

---
+++ wwwsrc/src/store.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
+
+Vue.use(Vuex)
+
+let baseUrl = location.host.includes('localhost') ? '//localhost:5000/' : '/'
+
+let auth = Axios.create({
+  baseURL: baseUrl + "account/",
+  timeout: 3000,
+  withCredentials: true
+})
+
+let api = Axios.create({
+  baseURL: baseUrl + "api/",
+  timeout: 3000,
+  withCredentials: true
+})
+
+export default new Vuex.Store({
+  state: {
+    user: {}
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user
+    }
+  },
+  actions: {
+    register({ commit, dispatch }, newUser) {
+      auth.post('register', newUser)
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'home' })
+        })
+        .catch(e => {
+          console.log('[registration failed] :', e)
+        })
+    },
+    authenticate({ commit, dispatch }) {
+      auth.get('authenticate')
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'home' })
+        })
+        .catch(e => {
+          console.log('not authenticated')
+        })
+    },
+    login({ commit, dispatch }, creds) {
+      auth.post('login', creds)
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'home' })
+        })
+        .catch(e => {
+          console.log('Login Failed')
+        })
+    }
+  }
+})
